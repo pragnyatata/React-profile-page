f298f3ee4d3547f388c74ef3342404386937fa1d
tata2501 <pragnya25012000@gmail.com>
2020-05-09 13:37:44 +0530

    Updates

---
+++ precache-manifest.268078267245b03f0de928ba6fd39b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e048c3ca54b50075da0bb93c39b38ec",
+    "url": "/React-profile-page/index.html"
+  },
+  {
+    "revision": "4f6935d08bb69ee16ac5",
+    "url": "/React-profile-page/static/css/main.6c05076e.chunk.css"
+  },
+  {
+    "revision": "a94f0177b43af18d81d2",
+    "url": "/React-profile-page/static/js/2.b1bda2b4.chunk.js"
+  },
+  {
+    "revision": "4f6935d08bb69ee16ac5",
+    "url": "/React-profile-page/static/js/main.2ee57d0e.chunk.js"
+  },
+  {
+    "revision": "ac2491f6b240d5d2d95c",
+    "url": "/React-profile-page/static/js/runtime~main.043b9a39.js"
+  }
+]);
